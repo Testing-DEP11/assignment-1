dddffb1570c977f11ec6b149bea89fbb17e511b1
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-07-29 23:09:18 +0530

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1{
+    public static void main(String[] args) {
+        
+        double accBalance = 123456.7890;
+
+        System.out.printf("Account Balace = $%,.2f\n", accBalance);
+
+    }
+}
